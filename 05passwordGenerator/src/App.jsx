0df3fa85afde299bfeb55edf926cbe0d490c3933
Vivek Wanve <wanvevivek123@gmail.com>
2024-03-04 18:18:03 +0530

    Removed +1 from passwordGenerator function

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     if(charAllowed) str = str + "!@#$%^&*-_+=[]{}~`"
 
     for(let i=1; i<=length; i++){
-      let ind = Math.floor(Math.random()*str.length + 1)
+      let ind = Math.floor(Math.random()*str.length)
       pass = pass + str.charAt(ind)
     }
 
