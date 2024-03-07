2a2cf05719a4506275fad90a3845746652616b9e
Vivek Wanve <wanvevivek123@gmail.com>
2024-03-07 19:21:49 +0530

    Add files from vscode using git

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-semibold text-white text-4xl my-3">Manage my Todos</h1>
+      <AddTodo/>
+    </>
+  )
+}
+
+export default App
---
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { addTodo, removeTodo, updateTodo } from '../features/todo/todoSlice'
+
+function AddTodo() {
+
+  const [input, setInput] = useState('')
+  const [updateId, setUpdateId] = useState("")
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  const addTodoHandler = (e) => {
+    e.preventDefault()
+
+    if(toggleBtn.textContent === "Add Todo"){
+      dispatch(addTodo(input))
+    }else{
+      dispatch(updateTodo({id: updateId, text: input}))
+      toggleBtn.textContent = "Add Todo"
+    }
+
+    setInput('')
+  }
+
+  return (
+    <>
+      <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+        <input
+          type="text"
+          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          placeholder="Enter a Todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          id='toggleBtn'
+          type="submit"
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Add Todo
+        </button>
+      </form>
+
+      <div className='font-medium text-white text-2xl my-3 py-2'>Todos</div>
+
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className='text-white'>{todo.text}</div>
+
+            <div className='flex justify-between items-center'>
+
+              <button className='mx-4 py-1 px-4 bg-yellow-300 hover:bg-yellow-400 rounded focus:outline-none text-md focus:ring-2 focus:ring-white'
+              onClick={() => {
+                setInput(todo.text)
+                toggleBtn.textContent = "Update Todo"
+                setUpdateId(todo.id)
+              }}>
+                ✏️
+              </button>
+
+              <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+                className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+
+            </div>
+            
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default AddTodo
+
